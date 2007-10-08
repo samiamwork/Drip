811ee93cd3d595797b6dc8a4e982cd084133bc61
Nur Monson <samiamwork@gmail.com>
2007-10-07 20:45:03 -0700

    Added a small icon in the corner of the new color well so that it's more clear what it is.

--- Source/CleanColorWell.m
+++ Source/CleanColorWell.m
@@ -8,16 +8,37 @@
 
 #import "CleanColorWell.h"
 
-
 @implementation CleanColorWell
 
 - (void)drawRect:(NSRect)aRect
 {
-	[[self color] set];
-	NSRectFill(aRect);
-	[[NSColor colorWithCalibratedWhite:0.6f alpha:1.0f] setStroke];
+	NSRect bounds = [self bounds];
+	[[self color] setFill];
+	NSRectFill( bounds );
 	CGContextRef cxt = [[NSGraphicsContext currentContext] graphicsPort];
-	NSRect insetRect = NSInsetRect(aRect,0.5f,0.5f);
+
+	NSRect tinyRect = NSMakeRect(5.0f,5.0f,5.0f,5.0f);
+	NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:tinyRect];
+	CGContextSetRGBStrokeColor( cxt, 0.6f,0.6f,0.6f, 1.0f);
+	[circle setLineWidth:3.0f];
+	[circle stroke];
+	[[NSColor redColor] setFill];
+	[circle fill];
+	
+	CGContextSaveGState( cxt );
+	CGContextTranslateCTM( cxt, 6.0f, 0.0f );
+	[circle stroke];
+	[[NSColor greenColor] setFill];
+	[circle fill];
+	
+	CGContextTranslateCTM( cxt, 6.0f, 0.0f );
+	[circle stroke];
+	[[NSColor blueColor] setFill];
+	[circle fill];
+	
+	CGContextRestoreGState( cxt );
+	[[NSColor colorWithCalibratedWhite:0.6f alpha:1.0f] setStroke];
+	NSRect insetRect = NSInsetRect(bounds,0.5f,0.5f);
 	CGContextStrokeRectWithWidth(cxt, *(CGRect *)&insetRect, 1.0f);
 }
 
