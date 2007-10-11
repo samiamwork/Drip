cc8e4e8d81f52f855a63829fcdf3929975036079
Nur Monson <samiamwork@gmail.com>
2007-10-10 23:55:49 -0700

    Changed the stroke of the three circles in the color well into a shadow.

--- Source/CleanColorWell.m
+++ Source/CleanColorWell.m
@@ -17,26 +17,28 @@
 	NSRectFill( bounds );
 	CGContextRef cxt = [[NSGraphicsContext currentContext] graphicsPort];
 
-	NSRect tinyRect = NSMakeRect(5.0f,5.0f,5.0f,5.0f);
-	NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:tinyRect];
-	CGContextSetRGBStrokeColor( cxt, 0.6f,0.6f,0.6f, 1.0f);
-	[circle setLineWidth:3.0f];
-	[circle stroke];
-	[[NSColor redColor] setFill];
-	[circle fill];
-	
-	CGContextSaveGState( cxt );
-	CGContextTranslateCTM( cxt, 6.0f, 0.0f );
-	[circle stroke];
-	[[NSColor greenColor] setFill];
-	[circle fill];
-	
-	CGContextTranslateCTM( cxt, 6.0f, 0.0f );
-	[circle stroke];
-	[[NSColor blueColor] setFill];
-	[circle fill];
-	
-	CGContextRestoreGState( cxt );
+	CGContextSaveGState( cxt ); {
+		NSRect tinyRect = NSMakeRect(5.0f,5.0f,5.0f,5.0f);
+		NSBezierPath *circle = [NSBezierPath bezierPathWithOvalInRect:tinyRect];
+		NSShadow *shadow = [[NSShadow alloc] init];
+		[shadow setShadowColor:[NSColor colorWithCalibratedWhite:0.6f alpha:1.0f]];
+		[shadow setShadowOffset:NSMakeSize(0.0f,-1.5f)];
+		[shadow setShadowBlurRadius:1.0f];
+		[shadow set];
+		[shadow release];
+		CGContextSetRGBStrokeColor( cxt, 0.6f,0.6f,0.6f, 1.0f);
+		[circle setLineWidth:3.0f];
+		[[NSColor redColor] setFill];
+		[circle fill];
+		
+		CGContextTranslateCTM( cxt, 6.0f, 0.0f );
+		[[NSColor greenColor] setFill];
+		[circle fill];
+		
+		CGContextTranslateCTM( cxt, 6.0f, 0.0f );
+		[[NSColor blueColor] setFill];
+		[circle fill];
+	} CGContextRestoreGState( cxt );
 	[[NSColor colorWithCalibratedWhite:0.6f alpha:1.0f] setStroke];
 	NSRect insetRect = NSInsetRect(bounds,0.5f,0.5f);
 	CGContextStrokeRectWithWidth(cxt, *(CGRect *)&insetRect, 1.0f);
